5b1b305ec2dec04e59c06b5d3f563e3b353b1794
Kartikey1078 <kartiktyagi002@gmail.com>
2025-03-11 18:16:35 +0530

    working on secondPage

--- src/components/BlackOutName.jsx
+++ src/components/BlackOutName.jsx
@@ -3,10 +3,19 @@ import { useSpring, animated } from "@react-spring/web";
 
 function BlackOutName() {
   const [toggle, setToggle] = useState(false);
+  const [hide,setHide] = useState(false);
 
   useEffect(() => {
     setToggle(true); 
   }, []);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+        setHide(true)
+    },2500)
+  },[])
+
+ 
 
   const move = useSpring({
     from: {
@@ -23,18 +32,32 @@ function BlackOutName() {
         opacity: 1, 
         }
       : {}, 
-    config: { tension: 100, friction: 50 },
+    config: { tension: 100, friction: 10 },
+  });
+  
+
+  const moveUp = useSpring({
+    from:{
+        height:'100vh',
+
+    },
+     to: hide
+      ?{
+        height:'0'
+        }
+    : {},
+    config: { duration: 700 },
   });
 
   return (
-    <div className="relative w-[100vw] h-[100vh] bg-black">
+    <animated.div style={moveUp} className="relative w-[100vw]  bg-black">
       <animated.p
         style={{ position: "absolute", ...move }} 
         className="text-cyan-50 border-2 bgText border-white rounded-full p-3 w-[200px] text-center text-xl"
       >
         KARTIKEY TYAGÎ
       </animated.p>
-    </div>
+    </animated.div>
   );
 }
 
